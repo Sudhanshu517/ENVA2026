0820164b013a8b9db83feba5da606e560d5fe7b5
pranshr <pranshr005@gmail.com>
2026-02-04 00:47:04 +0530

    final carousel design

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import About from "./Components/About";
+import Events from "./Components/Events";
+
+
+function App() {
+
+  return (
+    <div className="bg-[#010101] flex flex-col items-center">
+      <About />
+      <Events />
+    </div>
+  )
+}
+
+export default App
